5d75bfaf1817b18329e62e06fdc85e85d7f78c2c
ALTRIN BENSER <altrin2404@gmail.com>
2026-02-23 10:50:39 +0530

    Add PrimeNumber Java file

---
+++ PrimeNumber.java
@@ -0,0 +1,24 @@
+public class PrimeNumber {
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int num = 19;
+
+        if (isPrime(num)) {
+            System.out.println(num + " is a Prime Number");
+        } else {
+            System.out.println(num + " is NOT a Prime Number");
+        }
+    }
+}
